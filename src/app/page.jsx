42c3fd26946c669e62bc790c5e82809e6d54e64e
MDalmi <maicondalmina.pf003@academico.ifsul.edu.br>
2024-10-01 10:13:23 -0300

    commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,4 @@
-import { getProdutosDB } from '@/bd/useCases/produtoUseCases';
+import { getProdutosDB } from '@/componentes/bd/usecases/produtoUseCases';
 import Link from 'next/link';
 
 export const revalidate = 60; // revalida a cada 60 segundos
@@ -22,9 +22,9 @@ export default async function Home() {
                 <div className="card-body ">
                   <h5 className="card-title">{objeto.nome}</h5>
                   <p className="card-text"><small className="text-muted">Preço: {objeto.valor}</small></p>
-                  <p className="card-text"><small className="text-muted">Categoria: {objeto.categoria_nome}</small></p>                  
+                  <p className="card-text"><small className="text-muted">Categoria: {objeto.categoria_nome}</small></p>
                 </div>
-                <div class="card-footer text-muted">                  
+                <div class="card-footer text-muted">
                   <Link type="button" className="btn btn-secondary" href={`/${objeto.codigo}/detalhe`}>Detalhes do produto</Link>
                 </div>
               </div>
