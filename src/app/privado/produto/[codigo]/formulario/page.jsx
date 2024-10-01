73cd4f01206874c52908f941931917bf2f1760e0
MDalmi <maicondalmina.pf003@academico.ifsul.edu.br>
2024-10-01 08:41:39 -0300

    commit

---
+++ src/app/privado/produto/[codigo]/formulario/page.jsx
@@ -0,0 +1,85 @@
+import { notFound, redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+import { getProdutoPorCodigoDB, addProdutoDB, updateProdutoDB }
+    from "@/componentes/bd/usecases/produtoUseCases";
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import { Suspense } from 'react';
+import Loading from '@/componentes/comuns/Loading';
+
+const FormularioPage = async ({ params }) => {
+    let produto = null;
+    if (params.codigo == 0) {
+        produto = { codigo: 0, nome: "" }
+    } else {
+        try {
+            produto = await getProdutoPorCodigoDB(params.codigo);
+        } catch (err) {
+            return notFound();
+        }
+    }
+
+    const salvarProduto = async (formData) => {
+        'use server';
+        const objeto = {
+            codigo: formData.get('codigo'),
+            nome: formData.get('nome')
+        }
+        try {
+            if (objeto.codigo == 0) {
+                await addProdutoDB(objeto);
+            } else {
+                await updateProdutoDB(objeto);
+            }
+        } catch (err) {
+            throw new Error('Erro: ' + err);
+        }
+        revalidatePath('/privado/produto/');
+        redirect('/privado/produto');
+    }
+
+    return (
+        <>
+            <Suspense fallback={<Loading />}>
+                <div style={{ textAlign: 'center' }}>
+                    <h2>Produto</h2>
+                </div>
+                <form action={salvarProduto}>
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-12 col-md-6">
+                                <div>
+                                    <FloatingLabel controlId="campoCodigo"
+                                        label="Código" className="mb-3">
+                                        <Form.Control type="number"
+                                            defaultValue={produto.codigo} readOnly
+                                            name="codigo" />
+                                    </FloatingLabel>
+                                </div>
+                                <div>
+                                    <FloatingLabel controlId="campoNome"
+                                        label="Nome" className="mb-3">
+                                        <Form.Control type="text"
+                                            defaultValue={produto.nome} required
+                                            name="nome" />
+                                    </FloatingLabel>
+                                </div>
+                                <div className="form-group text-center mt-3">
+                                    <button type="submit" className="btn btn-success">
+                                        Salvar <i className="bi bi-save"></i>
+                                    </button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </form>
+            </Suspense>
+        </>
+    )
+
+
+}
+
+export default FormularioPage;
---
+++ src/app/privado/produto/page.jsx
@@ -0,0 +1,76 @@
+import { Table } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { getProdutoDB, deleteProdutoDB } from "@/componentes/bd/usecases/produtoUseCases";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+import Link from "next/link";
+import { Suspense } from 'react';
+import Loading from '@/componentes/comuns/Loading';
+
+const deleteProduto = async (codigo) => {
+    'use server'
+    try {
+        await deleteProdutoDB(codigo);
+    } catch (err) {
+        console.log('Erro: ' + err);
+        throw new Error('Erro: ' + err);
+    }
+    revalidatePath('/privado/produto/');
+    redirect('/privado/produto/');
+}
+
+export default async function Categoria() {
+
+    revalidatePath('/privado/produto/');
+
+    const produtos = await getProdutoDB();
+
+    return (
+        <Suspense fallback={<Loading />}>
+            <div style={{ padding: '20px' }}>
+                <Link href={`/privado/produto/${0}/formulario`}
+                    className="btn btn-primary">
+                    <i className="bi bi-file-earmark-plus"></i> Novo
+                </Link>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th style={{ textAlign: 'center' }}>Ações</th>
+                            <th>Código</th>
+                            <th>Nome</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            produtos.map((produto) => (
+                                <tr key={produto.codigo}>
+                                    <td align="center">
+                                        <Link className="btn btn-info" title="Editar"
+                                            href={`/privado/produto/${produto.codigo}/formulario`}>
+                                            <i className="bi bi-pencil-square"></i>
+                                        </Link>
+                                        <form
+                                            action={deleteCategoria.bind(null, produto.codigo)}
+                                            className="d-inline">
+                                            <Button className="btn btn-danger" title="Excluir"
+                                                type="submit">
+                                                <i className="bi bi-trash"></i>
+                                            </Button>
+                                        </form>
+                                    </td>
+                                    <td>{produto.codigo}</td>
+                                    <td>{produto.nome}</td>
+                                    <td>{produto.estoque}</td>
+                                    <td>{produto.ativo}</td>
+                                    <td>{produto.categora_nome}</td>
+                                    
+                                </tr>
+                            ))
+                        }
+
+                    </tbody>
+                </Table>
+            </div>
+        </Suspense>
+    )
+}
